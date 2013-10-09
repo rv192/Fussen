2d45bdcc09337b392d90ecde7c54c110aeb032c7
rv192 <William@chenqiuyotekiMacBook-Air.local>
2013-10-09 23:19:55 +0800

    更新部分代码

--- Fussen/Fussen.Testing.Console/Program.cs
+++ Fussen/Fussen.Testing.Console/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using Fussen.Graphics;
 
@@ -15,3 +16,22 @@ namespace Fussen.Testing.Console
 		}
 	}
 }
+=======
+using System;
+using Fussen.Graphics;
+
+namespace Fussen.Testing.Console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ExifViewer info = new ExifViewer ("I:\\百度云\\德希游\\Photo2013\\20130923_1927_20-920T-3283.jpg");
+
+			System.Console.Write (info.ToString ());
+
+			System.Console.ReadLine ();
+		}
+	}
+}
+>>>>>>> d0cf1d69c01b64b1b46a9c789c1fbbbec5447d94
